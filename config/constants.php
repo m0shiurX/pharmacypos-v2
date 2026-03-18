d761a6c2865e2cdcf847d68c0638b7a99404064c
Moshiur Rahman <10681932+m0shiurX@users.noreply.github.com>
2026-03-18 23:26:20 +0600

    updated version

--- config/constants.php
+++ config/constants.php
@@ -19,7 +19,7 @@ return [
     'document_size_limit' => '5000000', // in Bytes,
     'image_size_limit' => '5000000', // in Bytes
 
-    'asset_version' => 671,
+    'asset_version' => 672,
 
     'disable_purchase_in_other_currency' => true,
 
